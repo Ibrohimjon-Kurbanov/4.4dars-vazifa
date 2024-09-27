a1c888b5b505aea9d47a5c80bc40fd29eea596cf
Ibrohimjon-Kurbanov <kurbanovolddev@gmail.com>
2024-09-28 00:30:26 +0200

    4.4dars-vazifa

---
+++ js/index.js
@@ -0,0 +1,193 @@
+// 1-MASALA
+// let num = +prompt("Sonni kiriting:");
+
+// if (num > 0) {
+//   console.log("Musbat son");
+// } else {
+//   console.log("Manfiy son");
+// }
+
+// 2-MASALA
+// let num = +prompt("Sonni kiriting");
+
+// if (num % 2 === 0) {
+//   console.log("Juft");
+// } else {
+//   console.log("Toq");
+// }
+
+// 3-MASALA
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 > num2) {
+//   console.log("Katta");
+// } else if (num1 === num2) {
+//   console.log("Teng");
+// } else {
+//   console.log("Kichik");
+// }
+
+// 4-MASALA
+
+// let height = +prompt("Bo'yini kiriting:");
+// let width = +prompt("Enini kiriting:");
+
+// if (height === width) {
+//   console.log("Kvadrat");
+// } else {
+//   console.log("Kvadrat emas");
+// }
+
+// 5-MASALA
+
+// let temperature = +prompt("Harorat sonini kiritng:");
+
+// if (temperature > 0) {
+//   console.log("Yozgi");
+// } else {
+//   console.log("Qishki");
+// }
+
+// 6-MASALA
+
+// let baho = +prompt("Bahoni kiriting:");
+
+// if (baho === 5) {
+//   console.log("A'lo");
+// } else if (baho === 4) {
+//   console.log("Yaxshi");
+// } else if (baho === 3) {
+//   console.log("Qoniqarli");
+// } else if (baho === 2) {
+//   console.log("Yomon");
+// } else if (baho === 1) {
+//   console.log("Juda yomon");
+// } else {
+//   console.log("To'gri baho kiriting !");
+// }
+
+// 7-MASALA
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let res = "";
+
+// if (num1 > num2) {
+//   if (num2 > num3) {
+//     res = num1;
+//   } else {
+//     res = num3;
+//   }
+// } else {
+//   if (num2 > num3) {
+//     res = num2;
+//   } else {
+//     res = num3;
+//   }
+// }
+// console.log(res);
+
+// 8-MASALA
+
+// let num = +prompt("Sonni kiritng:");
+// let res = String(num).length;
+
+// if (res === 5) {
+//   console.log("Besh xonali");
+// } else {
+//   console.log("Besh xonali emas");
+// }
+
+// 9-MASALA
+// let num = +prompt("Sonni kiritng:");
+// let res = "";
+
+// if (num >= 3 && num <= 5) {
+//   res = "Bahor";
+// } else if (num >= 6 && num <= 8) {
+//   res = "Yoz";
+// } else if (num >= 9 && num <= 11) {
+//   res = "Kuz";
+// } else if (num === 12 || num === 1 || num === 2) {
+//   res = "Qish";
+// } else {
+//   res = "Oy raqami mavjud emas";
+// }
+// console.log(res);
+
+// 10-MASALA
+
+// let num = +prompt("Sonni kiritng:");
+// let res = "";
+
+// if (num >= 1 && num <= 5) {
+//   res = "Ishchi kuni";
+// } else if (num >= 6 && num <= 7) {
+//   res = "Dam olish kuni";
+// } else {
+//   res = "Ish kuni mavjud emas";
+// }
+// console.log(res);
+
+// 11-MASALA
+
+// let age = +prompt("Yilingizni kiritng:");
+
+// if (age > 2005) {
+//   console.log("Yosh bola");
+// } else {
+//   console.log("Katta odam");
+// }
+
+// 12-MASALA
+
+// let num = +prompt("Soatni kiriting:");
+
+// if (num >= 8 && num <= 18) {
+//   console.log("Ish vaqti");
+// } else {
+//   console.log("Tanafus");
+// }
+
+// 13-MASALA
+
+// let num = +prompt("Sonni kiriting:");
+
+// if (num) {
+//   console.log("Bu raqam");
+// } else {
+//   console.log("Bu raqam emas");
+// }
+
+// 14-MASALA
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let res = "";
+
+// if (num1 >= num2 && num1 >= num3) {
+//   res = num1;
+// } else if (num2 >= num1 && num2 >= num3) {
+//   res = num2;
+// } else {
+//   res = num3;
+// }
+
+// if (res > 100) {
+//   console.log("Katta son");
+// } else {
+//   console.log("Kichik son");
+// }
+
+// 15-MASALA
+
+// let age = +prompt("Yoshingizni kiriting:");
+
+// if (age <= 18) {
+//   console.log("Tekin kirish");
+// } else {
+//   console.log("Pul to'lash kerak");
+// }
